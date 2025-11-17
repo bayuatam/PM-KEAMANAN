a7575da46e78428e4975d9dbdd21d3613e2aecd2
aapriliaaxyz-dev <aapriliaaxyz@gmail.com>
2025-11-17 17:52:29 +0700

    Skema 2: Penambahan Tujuan Aplikasi dan Info Versi (nisa)

--- app/Views/about/index.php
+++ app/Views/about/index.php
@@ -13,6 +13,12 @@
 
                 <p>Pengembangan aplikasi ini dilatarbelakangi oleh kebutuhan untuk memodernisasi pemasaran produk peternakan yang sebelumnya masih mengandalkan metode konvensional (pamflet, WhatsApp). Melalui platform digital ini, kami menargetkan peningkatan efisiensi operasional, kemudahan akses bagi civitas academica, dan penguatan citra Polinela sebagai institusi yang adaptif terhadap inovasi teknologi.</p>
                 
+                <h3 class="mt-5 mb-3 fw-bold">Tujuan Utama Aplikasi</h3>
+                <ul>
+                    <li>Meningkatkan jangkauan pemasaran produk peternakan kampus.</li>
+                    <li>Mempermudah proses transaksi bagi civitas academica.</li>
+                    <li>Mendukung inisiatif digitalisasi produk unit usaha Polinela.</li>
+                </ul>
                 <div class="card mt-5">
                     <div class="card-header">
                         <h5 class="mb-0">Tim Pengembang</h5>
@@ -23,6 +29,7 @@
                         <li class="list-group-item">Bayu Pratama (Database Engineer)</li>
                         <li class="list-group-item">Annisa Afrilia (Integrasi & Dokumentasi)</li>
                         <li class="list-group-item">ANGGOTA BAYANGAN (PERCOBAAN)</li>
+                        <li class="list-group-item bg-light">Versi Aplikasi: Beta 1.0 (MI 5A)</li>
                     </ul>
                 </div>
             </div>
